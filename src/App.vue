23aa0cf3506a602e7f3049c3f8540683db0fc454
ArdhianWisnu <wisnuardhian969@gmail.com>
2024-09-20 15:07:27 +0700

    first push

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import SiteNavigation from './components/SiteNavigation.vue';
+</script>
+
+<template>
+    <div class=" font-Roboto">
+     <SiteNavigation/>
+     <RouterView/>
+    </div>
+</template>
+
+<style>
+
+</style>
+
